6a56b838f62c0ab6b0674883ce281bcd0073f37b
husain3000 <73273019+husain3000@users.noreply.github.com>
2023-02-25 15:08:59 +0530

    first commit

---
+++ Convert to a string format.js
@@ -0,0 +1,7 @@
+// var date1 = new Date();
+// date1.toString();
+// console.log(date1);
+
+var date1 = new Date();
+date1.toTimeString();
+console.log(date1);
---
+++ Create a new Date object.js
@@ -0,0 +1,13 @@
+// var now = new Date();
+// now.toString() 
+// console.log(now.toString())
+
+/*it is printing current date and time*/
+
+// var epoch = new Date(0);
+// epoch.toISOString()
+// console.log(epoch);
+
+var anything = new Date('11/12/2012');
+anything.toString()
+console.log(anything);
